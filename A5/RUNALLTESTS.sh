d265237cc180a7fef65eb55f4029f5a6db7a7cfe
Kent-Liang <kentliang@hotmail.com>
2015-04-02 22:34:50 -0400

    remove accessing failed test case

--- A5/RUNALLTESTS.sh
+++ A5/RUNALLTESTS.sh
@@ -10,12 +10,4 @@ do
   echo
 done
 echo "------------------------------------------------------"
-echo "$(tput setaf 2)SHOULD FAIL TEST CASES:$(tput sgr0)"
-echo
-for program in `ls $WHERE/tests/failing/{,*/}*.488`
-do
-  echo "$(tput setaf 2)Testing: $program$(tput sgr0)"
-  ./RUNCOMPILER.sh $program
-  echo
-done
-echo "------------------------------------------------------"
+
